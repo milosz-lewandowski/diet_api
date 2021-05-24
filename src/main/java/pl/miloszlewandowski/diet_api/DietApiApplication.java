1b0c74b4d2c97aec6edadc502bc738e83927417f
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-24 23:33:14 +0200

    Initial commit - spring-boot starter project template

---
+++ src/main/java/pl/miloszlewandowski/diet_api/DietApiApplication.java
@@ -0,0 +1,13 @@
+package pl.miloszlewandowski.diet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietApiApplication.class, args);
+    }
+
+}
