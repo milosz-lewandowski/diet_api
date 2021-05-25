6e6f6b69d28610bed301a78bbf7648052346ac47
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-25 08:43:38 +0200

    set correct reference

--- src/main/java/pl/miloszlewandowski/diet_api/DietCalories.java
+++ src/main/java/pl/miloszlewandowski/diet_api/DietCalories.java
@@ -1,14 +1,9 @@
 package pl.miloszlewandowski.diet_api;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 
 @Data
-//@JsonIdentityInfo(
-//        generator = ObjectIdGenerators.PropertyGenerator.class,
-//        property = "id")
 public class DietCalories { //kaloryczność, np. 1500, 2000 kcal
     private Integer dietCaloriesId;
     private Integer calories;
