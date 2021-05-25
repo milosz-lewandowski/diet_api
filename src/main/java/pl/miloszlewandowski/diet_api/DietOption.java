b71d9e0cd42ab160a658cc6b9b005a90209164fc
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-25 08:39:07 +0200

    disable diet calories Reference

--- src/main/java/pl/miloszlewandowski/diet_api/DietOption.java
+++ src/main/java/pl/miloszlewandowski/diet_api/DietOption.java
@@ -1,23 +1,19 @@
 package pl.miloszlewandowski.diet_api;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 
 import java.util.Set;
 
+
 @Data
-//@JsonIdentityInfo(
-//        generator = ObjectIdGenerators.PropertyGenerator.class,
-//        property = "id")
 public class DietOption { //wariant diety, np. "5 posiłków" lub "3 posiłki"
     private Integer dietOptionId;
     private String name;
     private String abbreviation;
     @JsonManagedReference
     private Diet diet;
-    @JsonBackReference
-    private Set<DietCalories> dietCalories;
+//    @JsonBackReference
+//    private Set<DietCalories> dietCalories;
 }
