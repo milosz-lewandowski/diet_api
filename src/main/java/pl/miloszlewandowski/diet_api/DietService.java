40b1e1675063097a77eda2397d1e3d983cbcb51b
Miłosz Lewandowski <milosz.hg@gmail.com>
2021-05-25 08:35:18 +0200

    sample data load

--- src/main/java/pl/miloszlewandowski/diet_api/DietService.java
+++ src/main/java/pl/miloszlewandowski/diet_api/DietService.java
@@ -13,14 +13,17 @@ public class DietService {
     Set<Diet> dietsRepo;
     ObjectMapper objectMapper = new ObjectMapper();
 
-    public Set<Diet> getDietsRepo() {
-        if (dietsRepo == null) dietsRepo = new HashSet<>();
+    public Set<Diet> getDiets() {
+        if (dietsRepo == null) {
+            dietsRepo = new HashSet<>();
+//            loadSampleDiet();
+        }
         return dietsRepo;
     }
 
     @SneakyThrows
     public String getDietsJson() {
-        return objectMapper.writeValueAsString(getDietsRepo());
+        return objectMapper.writeValueAsString(getDiets());
     }
     @SneakyThrows
     public String getDietByIDJson(Integer id)  {
@@ -72,6 +75,42 @@ public class DietService {
         editedDiet.setDietOptions(
                 newDietData.getDietOptions());
         return editedDiet;
+    }
+
+    private void loadSampleDiet() {
+        //  create empty collections
+        Set<Diet> diets = new HashSet<>();
+        Set<DietOption> dietOptionSet1 = new HashSet<>();
+        Set<DietCalories> dietCaloriesSet1 = new HashSet<>();
+
+        // create Diet
+        Diet diet1 = new Diet();
+        diet1.setDietId(1);
+        diet1.setName("diet1");
+        diet1.setDescription("dieta 1");
+
+        //  create DietOption
+        DietOption dietOption1 = new DietOption();
+        dietOption1.setDietOptionId(1);
+        dietOption1.setDiet(diet1);
+        dietOption1.setAbbreviation("abbreviation1");
+
+        //  create DietCalories
+        DietCalories dietCalories1 = new DietCalories();
+        dietCalories1.setDietCaloriesId(1);
+        dietCalories1.setCalories(1000);
+//        dietCalories1.setDietOption(dietOption1);
+
+        //  fill collections
+        dietCaloriesSet1.add(dietCalories1);
+        dietOptionSet1.add(dietOption1);
+
+        //  set collections for instances
+//        dietOption1.setDietCalories(dietCaloriesSet1);
+        diet1.setDietOptions(dietOptionSet1);
+        diets.add(diet1);
+
+        setDietsRepo(diets);
     }
 
 }
